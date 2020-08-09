e021436d52d695e06ad3a9af0f240c04d359bc58
Daniel Vargas <dannyvargashb@gmail.com>
2020-08-08 17:56:27 -0700

    Rendered incidents onto heatmap

--- client/components/heat-map.jsx
+++ client/components/heat-map.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
+
 export default class HeatMap extends React.Component {
   constructor(props) {
     super(props);
     this.state = { crimes: [], coords: [] };
     this.googleMapContainerRef = React.createRef();
-
+    this.displayHeatMap = this.displayHeatMap.bind(this);
     this.getData = this.getData.bind(this);
-    this.getPoints = this.getPoints.bind(this);
   }
 
   getData() {
@@ -18,55 +18,39 @@ export default class HeatMap extends React.Component {
         for (let i = 0; i < data.incidents.length; i++) {
           const latitude = data.incidents[i].incident_latitude;
           const longitude = data.incidents[i].incident_longitude;
-          const cityCoords = { lat: latitude, lng: longitude, type: data.incidents[i].incident_offense_description, icon: '' };
+          const cityCoords = [latitude, longitude];
           cityArray.push(cityCoords);
         }
         this.setState({ crimes: data.incidents, coords: cityArray });
       });
   }
 
-  getPoints() {
-    const gradient = [
-      'rgba(0, 255, 255, 0)',
-      'rgba(0, 255, 255, 1)',
-      'rgba(0, 191, 255, 1)',
-      'rgba(0, 127, 255, 1)',
-      'rgba(0, 63, 255, 1)',
-      'rgba(0, 0, 255, 1)',
-      'rgba(0, 0, 223, 1)',
-      'rgba(0, 0, 191, 1)',
-      'rgba(0, 0, 159, 1)',
-      'rgba(0, 0, 127, 1)',
-      'rgba(63, 0, 91, 1)',
-      'rgba(127, 0, 63, 1)',
-      'rgba(191, 0, 31, 1)',
-      'rgba(255, 0, 0, 1)'
-    ];
-
+  displayHeatMap() {
     const coords = this.state.coords;
-    for (let i = 0; i < coords.length; i++) {
-      return [
-        new google.maps.LatLng(coords[i].lat, coords[i].lng)
-      ];
-    }
+    coords.map(coord => {
+      this.heatmap = new google.maps.visualization.HeatmapLayer({
+        data: [new google.maps.LatLng(coord[0], coord[1])],
+        map: this.map
+      });
+    });
   }
 
   componentDidMount() {
     /* global google */
     // marking as global because it should be in a script tag in the HTML file!
+    const coords = this.state.coords;
+
     this.map = new google.maps.Map(this.googleMapContainerRef.current, {
       zoom: 13,
       center: {
         lat: 34.052235,
         lng: -118.243683
       },
+      // mapTypeId: 'satellite',
       disableDefaultUI: true
     });
-    this.heatmap = new google.maps.visualization.HeatmapLayer({
-      // data: this.getPoints(),
-      map: this.map
-    });
     this.getData();
+
   }
 
   render() {
@@ -84,6 +68,7 @@ export default class HeatMap extends React.Component {
           style={{ width: '100vw', height: '100vh' }}
           className="d-flex"
         >
+          {this.displayHeatMap()}
         </div>
       </>
 
