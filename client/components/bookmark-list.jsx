5b6ae7f42b6493d67c78c0704b4c5ae4ee464bf9
Jeho Kim <jehokim90@gmail.com>
2020-08-12 06:50:44 +0000

    bookmark

--- client/components/bookmark-list.jsx
+++ client/components/bookmark-list.jsx
@@ -5,8 +5,7 @@ export default function BookmarkList(props) {
   const bookmarks = props.bookmarks;
   const bookmarkListItems = bookmarks.map((bookmark, i) => {
     const details = bookmark.incident_offense_description;
-    const date = bookmark.incident_date;
-    const newDate = date.slice(0, date.indexOf('T'));
+    const newDate = bookmark.incident_date;
     const address = bookmark.incident_address;
 
     return (
